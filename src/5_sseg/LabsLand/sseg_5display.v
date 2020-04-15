51c945e3a37e57f3b85682efd7bf260dd7a2c6f9
Andrés Cubillos <acubillos@unal.edu.co>
2020-04-14 23:48:13 -0500

    labsland

--- src/5_sseg/LabsLand/sseg_5display.v
+++ src/5_sseg/LabsLand/sseg_5display.v
@@ -1,25 +1,19 @@
 module sseg_5display( 
-
     input wire [15:0] V_SW,
-    input wire G_CLOCK_50, 
-    output wire [6:0] G_HEX0,
-    output wire [6:0] G_HEX1,
-    output wire [6:0] G_HEX2,
-    output wire [6:0] G_HEX3,
-    output wire [6:0] G_HEX4);
-    
-    wire enable;
-    
+    output wire [6:0] G_HEX0=0,
+    output wire [6:0] G_HEX1=0,
+    output wire [6:0] G_HEX2=0,
+    output wire [6:0] G_HEX3=0,
+    output wire [6:0] G_HEX4=0);
     
     reg [3:0] BCD1;
     reg [3:0] BCD2;
     reg [3:0] BCD3;
     reg [3:0] BCD4;
     reg [3:0] BCD5;
-    reg [22:0] cfreq = 0;
-    reg [2:0] count = 0;
-    reg [19:0] allBCD ;
-    reg [35:0] shifter ; 
+   
+    reg [19:0] allBCD =0;
+    reg [35:0] shifter=0 ; 
     integer i; 
    
     BCDtoSSeg bcdtosseg( .BCD(BCD1), .SSeg(G_HEX0) );
@@ -28,8 +22,9 @@ module sseg_5display(
     BCDtoSSeg bcdtosseg3( .BCD(BCD4), .SSeg(G_HEX3) );
     BCDtoSSeg bcdtosseg4( .BCD(BCD5), .SSeg(G_HEX4) );
    
-    always@(posedge enable ) begin 
-
+    always@( V_SW ) begin 
+        
+        shifter=16'b0000000000000000;
         shifter[15:0] = V_SW; 
     
         for (i = 0; i < 16; i = i+1) begin 
@@ -44,32 +39,12 @@ module sseg_5display(
         end  
 
         allBCD = shifter[35:16];
+        BCD1 <= allBCD[3:0]; 
+        BCD2 <= allBCD[7:4];  
+        BCD3 <= allBCD[11:8]; 
+        BCD4 <= allBCD[15:12];  
+        BCD5 <= allBCD[19:16];
     
-    end
-    
-    assign enable = cfreq[15];
-    
-    always @( posedge G_CLOCK_50 ) begin
-        
-	   cfreq <= cfreq+1;
-
-    end
-
-    always @( posedge enable ) begin
-	  
-        case ( count ) 
-				
-            0: begin BCD1 <= allBCD[3:0]; end 
-            1: begin BCD2 <= allBCD[7:4]; end 
-            2: begin BCD3 <= allBCD[11:8]; end 
-            3: begin BCD4 <= allBCD[15:12]; end 
-            4: begin BCD5 <= allBCD[19:16]; end 
-			
-        endcase   
-           
-        if( count == 3'b100 ) count <= 3'b000;
-        else count <= count+1;
-           
     end
 
 endmodule
